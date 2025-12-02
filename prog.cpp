899fe89b27d23e38e3436495a6970d69976cad1a
Peter_Pert <petrtfx456@gmail.com>
2025-12-02 16:04:07 +0300

    сделал файл prog.cpp

---
+++ prog.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int numbers[] = { 12, 45, 7, 89, 23 };
+    int max = numbers[0];
+
+    for (int i = 1; i < 5; i++) {
+        if (numbers[i] > max) max = numbers[i];
+    }
+
+    cout << max << endl;
+    return 0;
+}
